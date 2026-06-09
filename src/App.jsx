d79162890d1774b79483efddb28eafce52d4623a
confiance4 <confiancehoungan4@gmail.com>
2026-06-09 21:55:44 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Product Analyzer</h1>
+      <p>App is working!</p>
+    </div>
+  )
+}
